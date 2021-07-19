af1242bd6d388e6f9066476059658e8d5a54241e
David Carmona <davidwalker2235@gmail.com>
2021-07-19 18:07:52 +0200

    add connection mapping

--- ChatSample/Hubs/ChatHub.cs
+++ ChatSample/Hubs/ChatHub.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ChatSample.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
         public async Task Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public async Task AddUserToList(string user)
+        {
+            IEnumerable<string> userList = _connections.GetNames();
+                _connections.Add(user, Context.ConnectionId);
+        }
+
+        public async Task SendUserList()
+        {
+            IEnumerable<string> userList =_connections.GetConnections("David");
+            await Clients.All.SendAsync("broadcastUserList", userList);
         }
     }
 }
---
+++ ChatSample/Hubs/ConnectionMapping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatSample.Hubs
+{
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, HashSet<string>> _connections =
+    new Dictionary<T, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public async void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(T key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public List<T> GetNames()
+        {
+            return _connections.Select(kvp => kvp.Key).ToList();
+        }
+
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+    }
+}
