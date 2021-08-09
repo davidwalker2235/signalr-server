a48652fd689badb1fa1076d129d3e5a2b4ebb67a
davidwalker2235 <davidwalker2235@gmail.com>
2021-08-09 17:17:08 +0200

    add run screen

---
+++ ChatSample/wwwroot/Scripts/EnterNameScreen.js
@@ -0,0 +1,37 @@
+﻿var renderInputName = () => {
+    let enterYourNameText = document.createElement('div');
+    enterYourNameText.setAttribute("class", "enterYourNameText");
+    enterYourNameText.innerHTML = 'Enter Your Name';
+
+    var logoRoot = document.createElement('div');
+    logoRoot.setAttribute("class", "logoRoot");
+    var logoImage = document.createElement('img');
+    logoImage.setAttribute("class", "logoImage");
+    logoImage.setAttribute("src", "ERNILogo.png");
+    logoRoot.appendChild(logoImage);
+
+    let inputName = document.createElement("INPUT");
+    inputName.setAttribute("class", "nameInput");
+    inputName.setAttribute("type", "text");
+    inputName.setAttribute("id", "userName");
+
+    let btn = document.createElement("button");
+    btn.className = "playButton";
+    btn.innerHTML = "PLAY!";
+    btn.type = "submit";
+    btn.name = "formBtn";
+    btn.id = "sendUserName";
+
+    var coverImage = document.createElement('img');
+    coverImage.setAttribute("class", "coverImage");
+
+    var inputNameRoot = document.createElement('div');
+    inputNameRoot.setAttribute("class", "nameRoot");
+    inputNameRoot.setAttribute("id", "inputNameRoot");
+    inputNameRoot.appendChild(logoRoot);
+    inputNameRoot.appendChild(enterYourNameText);
+    inputNameRoot.appendChild(inputName);
+    inputNameRoot.appendChild(btn);
+    document.getElementById('container').appendChild(coverImage);
+    document.getElementById('container').appendChild(inputNameRoot);
+}
---
+++ ChatSample/wwwroot/Scripts/SignalRScripts.js
@@ -0,0 +1,17 @@
+﻿var updateName = (connection) => {
+    let name = document.getElementById('userName').value;
+    if (name || !name.length === 0) {
+        connection.invoke('addUserToList', name);
+        connection.invoke('sendUserNames');
+        userName = name;
+
+        renderRunScreen(connection);
+    }
+}
+
+var sendSingleUserValue = (event, connection, userName) => {
+    // Call the Send method on the hub.
+    connection.invoke('sendSingleUserValue', userName);
+
+    event.preventDefault();
+}
